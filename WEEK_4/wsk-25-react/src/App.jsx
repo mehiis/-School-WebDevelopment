4cc6eb10ad287643ac5d10cd3901aa0af488a28e
beeboy98 <niko.mehilainen@gmail.com>
2025-04-07 14:06:56 +0300

    add testing component for future testing

--- WEEK_4/wsk-25-react/src/App.jsx
+++ WEEK_4/wsk-25-react/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from './components/Home.jsx';
+import Test from './components/Test.jsx';
 
 const App = function () {
   return (
     <>
       <h1>MY APP</h1>
       <Home />
+      <Test />
     </>
   );
 };
